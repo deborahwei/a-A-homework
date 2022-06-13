6c8216f23c6a76454fdbe18daf0174c797aec788
Deborah Wei <dew354@nyu.edu>
2022-06-13 17:47:42 -0400

    Finished initialize and valid_move and make_move

--- W4/W4D1/mancala/lib/board.rb
+++ W4/W4D1/mancala/lib/board.rb
@@ -1,21 +1,48 @@
+require 'byebug'
+
 class Board
   attr_accessor :cups
 
+
   def initialize(name1, name2)
+    @cups = Array.new(14) 
+    self.place_stones
   end
 
   def place_stones
-    # helper method to #initialize every non-store cup with four stones each
+    @cups.map!.with_index do |cup, i| 
+      if i == 6 || i == 13
+        []
+      else 
+        [:stone, :stone, :stone, :stone]
+      end
+    end
   end
 
   def valid_move?(start_pos)
+    raise "Invalid starting cup" if !start_pos.between?(0, 13)
+    raise "Starting cup is empty" if cups[start_pos].empty?
   end
 
   def make_move(start_pos, current_player_name)
+    stones = cups[start_pos].length
+    cups[start_pos] = []
+    i = start_pos
+    until stones == 0
+      i = (i + 1) % cups.length # goes to the next cup
+      if i != 13
+        cups[i] << :stone # add stone if it is not opponents cup
+        stones -= 1 
+      end
+    end
+    self.render
+    self.next_turn(i)
   end
 
   def next_turn(ending_cup_idx)
-    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    return :prompt if ending_cup_idx == 6
+    return :switch if cups[ending_cup_idx].length == 1
+    return ending_cup_idx if cups[ending_cup_idx].length > 1
   end
 
   def render
@@ -27,8 +54,14 @@ class Board
   end
 
   def one_side_empty?
+    return [0..5].all? {|i| cup[i].empty?}
+    return [7..12].all? {|i| cup[i].empty?}
   end
 
   def winner
+    return :draw if 
   end
 end
+
+# b = Board.new("D", "K")
+# b.make_move(3, "K")
