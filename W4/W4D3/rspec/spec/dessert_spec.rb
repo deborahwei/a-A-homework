3d2d047cfbee11e27ced132d2e4381ebc79179be
Deborah Wei <dew354@nyu.edu>
2022-06-15 09:48:10 -0400

    Added all the specs

--- W4/W4D3/rspec/spec/dessert_spec.rb
+++ W4/W4D3/rspec/spec/dessert_spec.rb
@@ -6,37 +6,62 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
-  let(:chef) { double("chef") }
+  subject(:chef) {double("chef", name: "Charlie")}
+  subject(:cake) {Dessert.new("baked good", 5, chef)}
 
   describe "#initialize" do
     it "sets a type"
+      expect(cake.type).to eq("baked good")
+    end
 
-    it "sets a quantity"
+    it "sets a quantity" do 
+      expect(cake.quantity).to eq(5)
+    end
 
-    it "starts ingredients as an empty array"
+    it "starts ingredients as an empty array" do 
+      expect(cake.ingredients).to be_empty
+    end
 
-    it "raises an argument error when given a non-integer quantity"
+    it "raises an argument error when given a non-integer quantity" do 
+      expect { Dessert.new("cake", "lots and lots", chef)}.to raise_error(Argument Error)
+    end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do 
+      cake.add_ingredient("flour")
+      expect(cake.ingredients).to include("flour")
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do 
+      ingredients = ["flour", "eggs", "milk", "butter"]
+      cake.ingredients = ingredients
+      cake.mix!
+      expect(cake.ingredients).not_to eq(ingredients)
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity" do 
+      cake.eat(2)
+      expect(cake.quantity).to eq(7)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do
+      cake.quantity = 5
+      expect {cake.eat(6)}.to raise_error("the amount is greater than the quanity")
+    end
   end
 
   describe "#serve" do
     it "contains the titleized version of the chef's name"
+    expect(cake.serve).to include(chef.name.titleize)
   end
 
   describe "#make_more" do
     it "calls bake on the dessert's chef with the dessert passed in"
+    expect(chef).to receive(:bake).with(cake)
   end
 end
