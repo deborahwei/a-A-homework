727dee1166f2705dcdd386d50cab29b961333bc2
Deborah Wei <dew354@nyu.edu>
2022-06-21 22:59:34 -0400

    Im done

--- W5/W5D4/into_to_rails/db/schema.rb
+++ W5/W5D4/into_to_rails/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_22_024745) do
+ActiveRecord::Schema.define(version: 2022_06_22_025211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "houses", force: :cascade do |t|
+    t.string "address", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string "name", null: false
