1e27afc21917de3e059b5f81e53c5617f935ff8f
Deborah Wei <dew354@nyu.edu>
2022-07-05 17:29:57 -0400

    Finished W7D2

--- W7/W7D2/helpers_layouts_partials/app/controllers/users_controller.rb
+++ W7/W7D2/helpers_layouts_partials/app/controllers/users_controller.rb
@@ -2,6 +2,15 @@ class UsersController < ApplicationController
   before_action :require_no_user!
 
   def create
+    u = User.create(user_params)
+
+    msg = UserMailer.welcome_email(u)
+    msg.deliver_now
+
+    render :root
+  end
+
+  def create
     @user = User.new(user_params)
     if @user.save
       login_user!(@user)
