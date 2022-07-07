9295731feb7cb4e46ddedfbe20c5bcf22f81a16c
Deborah Wei <dew354@nyu.edu>
2022-07-06 20:18:21 -0400

    lol w7d3

--- W7/W7D3/music_app/spec/models/user_spec.rb
+++ W7/W7D3/music_app/spec/models/user_spec.rb
@@ -1,6 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+describe User do 
+  subject(:cake) do 
+    FactoryBot.build(:user, email: "deborah@gmail.com", password: "starwars")
+  end
+
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password_digest) }
+  it { should validate_length_of(:password).is_at_least(6)}
+
+
+  describe "#is_password?" do 
+    it 'checks if the password matches' do
+      expect(user.is_password?("starwars")).to be true
+    end 
+  end
+
+  describe "reset_session_token" do
+    it 'resets the users session token' do 
+      expect()
+    end
+  end
 end
