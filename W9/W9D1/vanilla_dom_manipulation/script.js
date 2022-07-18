fc151d181f7ac4736b5866384adb2791fa267c63
Deborah Wei <dew354@nyu.edu>
2022-07-18 16:51:24 -0400

    done w9d2'

--- W9/W9D1/vanilla_dom_manipulation/script.js
+++ W9/W9D1/vanilla_dom_manipulation/script.js
@@ -18,18 +18,57 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // adding SF places as list items
 
-  const favoriteInput = document.querySelector(".favorite-input");
-  
-  const newListLi = document.createElement("li");
-  newListLi.textContent = favorite;
+  const handleFavoriteSubmit = (e) => {
+    e.preventDefault();
 
+    const favoriteInput = document.querySelector(".favorite-input");
+    const favorite = favoriteInput.value;
+    favoriteInput.value = "";
+
+    const newListLi = document.createElement("li");
+    newListLi.textContent = favorite;
+
+    const favoritesList = document.getElementById("sf-places");
+    favoritesList.appendChild(newListLi);
+
+  }
+
+  const listSubmitButton = document.querySelector(".favorite-submit");
+  listSubmitButton.addEventListener("click", handleFavoriteSubmit);
 
 
 
   // adding new photos
+  
+  const addNewPhotos = (e) => { 
+    const hiddenPhotoForm = document.querySelector(".photo-form-container");
+    if (hiddenPhotoForm.className === "photo-form-container") {
+      hiddenPhotoForm.className = 'photo-form-container hidden'
+    }
+    else { 
+      hiddenPhotoForm.className = 'photo-form-container'
+    }
+  }
 
-  // --- your code here!
+  const handlePhotoSubmit = (e) => { 
+    e.preventDefault();
 
+    const photoUrlInput = document.querySelector('photo-url-input')
+    const photo = photoUrlInput.value
+    photoUrlInput.value = ''
+
+    const newImage = document.createElement('img')
+    newImage.src = photo
+
+    const newPhotoLi = document.createElement("li");
+    newPhotoLi.appendChild(newImage);
+
+    const photosList = document.querySelector("dog-photos");
+    photosList.appendChild(newPhoto); 
+  }
+
+  const photoSubmitButton = document.querySelector(".photo-url-submit");
+  photoSubmitButton.addEventListener("click", handlePhotoSubmit);
 
 
 });
