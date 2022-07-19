bdd8bd2f8581591e3acbef9a37bcd85e1eb0ef7b
Deborah Wei <dew354@nyu.edu>
2022-07-19 19:54:37 -0400

    Finished W9D3

--- W9/W9D3/first_AJAX_request/js/index.js
+++ W9/W9D3/first_AJAX_request/js/index.js
@@ -4,4 +4,25 @@ console.log("Hello from the JavaScript console!");
 
 // Add another console log here, outside your AJAX request
 
+$.ajax({
+    url: 'http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b',
+    type: 'GET',
+    data: {
+      widget: {
+        name: 'The Best Widget',
+        maker: 'The Widget King'
+      }
+    },
+    success(data) {
+      console.log(data);
+      // `create` action should `render json: @widget`
+      // this gives the client access to the `id` attribute issued by
+      // the server.
+    },
+    error() {
+      console.log('Error');
+    }
+  });
 
+  console.log('hi')
+  
